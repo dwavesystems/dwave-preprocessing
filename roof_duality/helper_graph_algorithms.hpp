15be11dd4bfee872e576bce32c797c27a322fc4d
Anil Mahmud <amahmud@dwavesys.com>
2020-12-01 08:29:37 -0800

    NFC

--- roof_duality/helper_graph_algorithms.hpp
+++ roof_duality/helper_graph_algorithms.hpp
@@ -27,12 +27,136 @@
 
 #include "helper_data_structures.hpp"
 
+// Perform breadth first search from a certain vertex, a depth equal to  the
+// number of vertices means that vertex could not be reached from the
+// start_vertex, since the maximum depth can be equal to number of vertices -1.
+int breadthFirstSearch(std::vector<std::vector<int>> &adjacency_list,
+                       int start_vertex, std::vector<int> &depth_values,
+                       bool print_result = false) {
+  int num_vertices = adjacency_list.size();
+  int UNVISITED = num_vertices;
+  vector_based_queue<int> vertex_queue(num_vertices);
+  depth_values.resize(num_vertices);
+  std::fill(depth_values.begin(), depth_values.end(), UNVISITED);
+
+  depth_values[start_vertex] = 0;
+  vertex_queue.push(start_vertex);
+
+  while (!vertex_queue.empty()) {
+    int parent_vertex = vertex_queue.pop();
+    int current_depth = depth_values[parent_vertex] + 1;
+    auto eit = adjacency_list[parent_vertex].begin();
+    auto eit_end = adjacency_list[parent_vertex].end();
+    for (; eit != eit_end; eit++) {
+      int to_vertex = *eit;
+      if (depth_values[to_vertex] == UNVISITED) {
+        depth_values[to_vertex] = current_depth;
+        vertex_queue.push(to_vertex);
+      }
+    }
+  }
+  return UNVISITED;
+}
+
+// Perform breadth first search on a residual graph from a certain vertex, a
+// depth equal to  the number of vertices means that vertex could not be reached
+// from the start_vertex, since the maximum depth can be equal to number of
+// vertices -1.
+// @param depth_values : reference to vector that should be filled with depth
+// values found by the search.
+// @param reverse : if set to true, the search does reverse breadth first
+// search, that is it tries to find if a certain vertex can reach the
+// start_vertex or not, In case of a flow graph the search should consider if
+// there is residual capacity in the reverse edge of the edge that connects the
+// parent to the child.
+// @returns the unreachable depth.
+template <class EdgeType>
+int breadthFirstSearchResidual(
+    std::vector<std::vector<EdgeType>> &adjacency_list, int start_vertex,
+    std::vector<int> &depth_values, bool reverse = false,
+    bool print_result = false) {
+  using capacity_t = typename EdgeType::capacity_type;
+  int num_vertices = adjacency_list.size();
+  int UNVISITED = num_vertices;
+  vector_based_queue<int> vertex_queue(num_vertices);
+  depth_values.resize(num_vertices);
+  std::fill(depth_values.begin(), depth_values.end(), UNVISITED);
+
+  depth_values[start_vertex] = 0;
+  vertex_queue.push(start_vertex);
+
+  // The check for whether the search should be reverse or not could be done
+  // inside the innermost loop, but that would be detrimental to performance.
+  if (reverse) {
+    while (!vertex_queue.empty()) {
+      int parent_vertex = vertex_queue.pop();
+      int current_depth = depth_values[parent_vertex] + 1;
+      auto eit = adjacency_list[parent_vertex].begin();
+      auto eit_end = adjacency_list[parent_vertex].end();
+      for (; eit != eit_end; eit++) {
+        int to_vertex = eit->to_vertex;
+        if (eit->getReverseEdgeResidual() &&
+            depth_values[to_vertex] == UNVISITED) {
+          depth_values[to_vertex] = current_depth;
+          vertex_queue.push(to_vertex);
+        }
+      }
+    }
+  } else {
+    while (!vertex_queue.empty()) {
+      int parent_vertex = vertex_queue.pop();
+      int current_depth = depth_values[parent_vertex] + 1;
+      auto eit = adjacency_list[parent_vertex].begin();
+      auto eit_end = adjacency_list[parent_vertex].end();
+      for (; eit != eit_end; eit++) {
+        int to_vertex = eit->to_vertex;
+        if (eit->residual && depth_values[to_vertex] == UNVISITED) {
+          depth_values[to_vertex] = current_depth;
+          vertex_queue.push(to_vertex);
+        }
+      }
+    }
+  }
+
+  if (print_result) {
+    std::vector<int> level_sizes;
+    std::vector<std::vector<int>> levels;
+    levels.resize(num_vertices + 1);
+    level_sizes.resize(num_vertices + 1, 0);
+    for (int i = 0; i < depth_values.size(); i++) {
+      level_sizes[depth_values[i]]++;
+    }
+    for (int i = 0; i < level_sizes.size(); i++) {
+      levels[i].reserve(level_sizes[i]);
+    }
+    for (int i = 0; i < depth_values.size(); i++) {
+      levels[depth_values[i]].push_back(i);
+    }
+    std::cout << std::endl;
+    std::cout << "Printing " << (reverse ? "reverse " : "")
+              << "breadth first search result starting from vertex : "
+              << start_vertex << std::endl;
+    std::cout << std::endl;
+    for (int i = 0; i < levels.size(); i++) {
+      if (!levels[i].size()) {
+        continue;
+      }
+      std::cout << "Level " << i << " has " << levels[i].size()
+                << " vertices : " << std::endl;
+      for (int j = 0; j < levels[i].size(); j++) {
+        std::cout << levels[i][j] << " ";
+      }
+      std::cout << std::endl;
+    }
+    std::cout << std::endl;
+  }
+
+  return UNVISITED;
+}
+
 // Check if the flow value in a given graph represented as an adjacency list is
 // valid or not.
-// @param adjacency_list the adjacency list of the flow network to test for.
-// @source the source considered for calcualting flow.
-// @sink the sink considered for calculating flow.
-// @return std::pair<the value of flow, if flow is valid or not>
+// @returns std::pair<the value of flow, if flow is valid or not>
 template <class EdgeType>
 std::pair<typename EdgeType::capacity_type, bool>
 isFlowValid(std::vector<std::vector<EdgeType>> &adjacency_list, int source,
@@ -125,94 +249,6 @@ isFlowValid(std::vector<std::vector<EdgeType>> &adjacency_list, int source,
   return {excess[sink], valid_flow};
 }
 
-// Perform breadth first search on a residual graph from a certain vertex, a
-// depth equal to  the number of vertices means that vertex could not be reached
-// from the start_vertex, since the maximum depth can be equal to number of
-// vertices -1.
-template <class EdgeType>
-int breadthFirstSearchResidual(
-    std::vector<std::vector<EdgeType>> &adjacency_list, int start_vertex,
-    std::vector<int> &depth_values, bool reverse = false,
-    bool print_result = false) {
-  using capacity_t = typename EdgeType::capacity_type;
-  int num_vertices = adjacency_list.size();
-  int UNVISITED = num_vertices;
-  vector_based_queue<int> vertex_queue(num_vertices);
-  depth_values.resize(num_vertices);
-  std::fill(depth_values.begin(), depth_values.end(), UNVISITED);
-
-  depth_values[start_vertex] = 0;
-  vertex_queue.push(start_vertex);
-
-  // The check for whether the search should be reverse or not could be done
-  // inside the innermost loop, but that would be detrimental to performance.
-  if (reverse) {
-    while (!vertex_queue.empty()) {
-      int v_parent = vertex_queue.pop();
-      int current_depth = depth_values[v_parent] + 1;
-      auto eit = adjacency_list[v_parent].begin();
-      auto eit_end = adjacency_list[v_parent].end();
-      for (; eit != eit_end; eit++) {
-        int to_vertex = eit->to_vertex;
-        if (eit->getReverseEdgeResidual() &&
-            depth_values[to_vertex] == UNVISITED) {
-          depth_values[to_vertex] = current_depth;
-          vertex_queue.push(to_vertex);
-        }
-      }
-    }
-  } else {
-    while (!vertex_queue.empty()) {
-      int v_parent = vertex_queue.pop();
-      int current_depth = depth_values[v_parent] + 1;
-      auto eit = adjacency_list[v_parent].begin();
-      auto eit_end = adjacency_list[v_parent].end();
-      for (; eit != eit_end; eit++) {
-        int to_vertex = eit->to_vertex;
-        if (eit->residual && depth_values[to_vertex] == UNVISITED) {
-          depth_values[to_vertex] = current_depth;
-          vertex_queue.push(to_vertex);
-        }
-      }
-    }
-  }
-
-  if (print_result) {
-    std::vector<int> level_sizes;
-    std::vector<std::vector<int>> levels;
-    levels.resize(num_vertices + 1);
-    level_sizes.resize(num_vertices + 1, 0);
-    for (int i = 0; i < depth_values.size(); i++) {
-      level_sizes[depth_values[i]]++;
-    }
-    for (int i = 0; i < level_sizes.size(); i++) {
-      levels[i].reserve(level_sizes[i]);
-    }
-    for (int i = 0; i < depth_values.size(); i++) {
-      levels[depth_values[i]].push_back(i);
-    }
-    std::cout << std::endl;
-    std::cout << "Printing " << (reverse ? "reverse " : "")
-              << "breadth first search result starting from vertex : "
-              << start_vertex << std::endl;
-    std::cout << std::endl;
-    for (int i = 0; i < levels.size(); i++) {
-      if (!levels[i].size()) {
-        continue;
-      }
-      std::cout << "Level " << i << " has " << levels[i].size()
-                << " vertices : " << std::endl;
-      for (int j = 0; j < levels[i].size(); j++) {
-        std::cout << levels[i][j] << " ";
-      }
-      std::cout << std::endl;
-    }
-    std::cout << std::endl;
-  }
-
-  return UNVISITED;
-}
-
 // Check if the flow value in a given graph represented as an adjacency list is
 // a valid max-flow or not and also return the flow value.
 template <class EdgeType>
@@ -235,16 +271,20 @@ isMaximumFlow(std::vector<std::vector<EdgeType>> &adjacency_list, int source,
 }
 
 // Tarzan's strongly connected component algoirhtm using iterative depth first
-// search. For more detail on the iterative version of the depth first search,
-// look at the comments in the iterative depth first search in the
-// convertPreflowToFlow function in push_relabel.hpp.
+// search. Note there will be no explicit stack used for this depth first
+// search, but instead the iterators pointing to the edges to be traversed for
+// each vertex will be updated and an array containing parents of vertices will
+// help simulate the stack. When we want to pop the stack we can basically look
+// at the parent of the current vertex being processed and when we want to push
+// a vertex into the stack, we can save it as the parent of the child vertex and
+// restart the loop with the child vertex as the current vertex.
 int stronglyConnectedComponents(std::vector<std::vector<int>> &adjacency_list,
                                 std::vector<int> &components) {
   int num_vertices = adjacency_list.size();
   using edge_iterator = typename std::vector<int>::iterator;
   components.resize(num_vertices);
   vector_based_stack<int> component_stack(num_vertices);
-  std::vector<bool> in_stack(num_vertices, false);
+  std::vector<bool> in_component_stack(num_vertices, false);
   int UNVISITED = num_vertices;
   std::vector<int> low_link_id(num_vertices, UNVISITED);
   std::vector<int> vertex_visit_id(num_vertices, UNVISITED);
@@ -270,8 +310,15 @@ int stronglyConnectedComponents(std::vector<std::vector<int>> &adjacency_list,
       low_link_id[current_vertex] = visit_id;
       visit_id++;
       component_stack.push(current_vertex);
-      in_stack[current_vertex] = true;
+      in_component_stack[current_vertex] = true;
+
       while (true) {
+
+        // We need to increment the actual iterator in the array. Think
+        // of recursive DFS, the counter of a for loop belonging to a particular
+        // stack frame is saved in the stack frame while the function calls
+        // itself recursively so when it comes back it finds the counter with
+        // the proper value.
         for (; pending_out_edges[current_vertex].first !=
                pending_out_edges[current_vertex].second;
              pending_out_edges[current_vertex].first++) {
@@ -281,13 +328,20 @@ int stronglyConnectedComponents(std::vector<std::vector<int>> &adjacency_list,
             low_link_id[child_vertex] = visit_id;
             visit_id++;
             component_stack.push(child_vertex);
-            in_stack[child_vertex] = true;
+            in_component_stack[child_vertex] = true;
 
-            // Recursive DFS call.
+            // Equivalent to calling DFS, i.e pushing into the stack. The
+            // while loop above will start the for loop all over again, but
+            // this time the current_vertex will be the child_vertex, and we
+            // will be able to trace back as the parent-child relationship is
+            // saved in the parent array. Note breaking the loop does not
+            // allow the iterator of edges to be incremented for the parent,
+            // just like in a recursive DFS, the for loop does not get
+            // incremented till the recursive call to DFS finishes.
             parent[child_vertex] = current_vertex;
             current_vertex = child_vertex;
             break;
-          } else if (in_stack[child_vertex]) {
+          } else if (in_component_stack[child_vertex]) {
             low_link_id[current_vertex] = std::min(low_link_id[current_vertex],
                                                    low_link_id[child_vertex]);
           }
@@ -300,11 +354,25 @@ int stronglyConnectedComponents(std::vector<std::vector<int>> &adjacency_list,
             int popped_vertex = -1;
             do {
               popped_vertex = component_stack.pop();
-              in_stack[popped_vertex] = false;
+              in_component_stack[popped_vertex] = false;
               components[popped_vertex] = num_strong_components;
             } while (popped_vertex != current_vertex);
             num_strong_components++;
           }
+
+          // If it is a root, then we break the while loop and the for loop
+          // above will automatically select the next vertex. It is similar to
+          // the situation that in DFS a wrapper function calls the first
+          // instance of DFS on different roots, and before the call there is no
+          // stack frame for DFS function. If it is not the root we need
+          // to simulate the popping of the stack, this we do by setting the
+          // current_vertex to be the parent of the child_vertex, we should
+          // also increment the edge iterator of the current vertex since in DFS
+          // we increment the iterator when the call to child DFS returns. The
+          // for loop will start again for the parent. Note: the current_vertex
+          // is changing within the loop so the edge iterator for the original
+          // current_vertex does not get incremented until and unless edges
+          // which cannot be traversed are encountered.
           if (current_vertex != root_vertex) {
             int completed_vertex = current_vertex;
             current_vertex = parent[current_vertex];
@@ -325,69 +393,39 @@ int stronglyConnectedComponents(std::vector<std::vector<int>> &adjacency_list,
   return num_strong_components;
 }
 
+// Transpose an adjacency list.
 void getTransposedAdjacencyList(std::vector<std::vector<int>> original,
                                 std::vector<std::vector<int>> &transposed) {
   int num_vertices = original.size();
   std::atomic<int> *transposed_sizes =
       (std::atomic<int> *)malloc(num_vertices * sizeof(std::atomic<int>));
-  for (int i = 0; i < num_vertices; i++) {
-    transposed_sizes[i] = 0;
+  for (int vertex = 0; vertex < num_vertices; vertex++) {
+    transposed_sizes[vertex] = 0;
   }
   transposed.resize(num_vertices);
 
 #pragma omp parallel for
-  for (int i = 0; i < num_vertices; i++) {
-    auto eit = original[i].begin();
-    auto eit_end = original[i].end();
+  for (int vertex = 0; vertex < num_vertices; vertex++) {
+    auto eit = original[vertex].begin();
+    auto eit_end = original[vertex].end();
     for (; eit != eit_end; eit++) {
       transposed_sizes[*eit]++;
     }
   }
 
-  for (int i = 0; i < num_vertices; i++) {
-    transposed[i].reserve(transposed_sizes[i]);
+  for (int vertex = 0; vertex < num_vertices; vertex++) {
+    transposed[vertex].reserve(transposed_sizes[vertex]);
   }
 
-  for (int i = 0; i < num_vertices; i++) {
-    auto eit = original[i].begin();
-    auto eit_end = original[i].end();
+  for (int vertex = 0; vertex < num_vertices; vertex++) {
+    auto eit = original[vertex].begin();
+    auto eit_end = original[vertex].end();
     for (; eit != eit_end; eit++) {
-      transposed[*eit].push_back(i);
+      transposed[*eit].push_back(vertex);
     }
   }
 
   free(transposed_sizes);
-}
-
-// Perform breadth first search from a certain vertex, a depth equal to  the
-// number of vertices means that vertex could not be reached from the
-// start_vertex, since the maximum depth can be equal to number of vertices -1.
-int breadthFirstSearch(std::vector<std::vector<int>> &adjacency_list,
-                       int start_vertex, std::vector<int> &depth_values,
-                       bool print_result = false) {
-  int num_vertices = adjacency_list.size();
-  int UNVISITED = num_vertices;
-  vector_based_queue<int> vertex_queue(num_vertices);
-  depth_values.resize(num_vertices);
-  std::fill(depth_values.begin(), depth_values.end(), UNVISITED);
-
-  depth_values[start_vertex] = 0;
-  vertex_queue.push(start_vertex);
-
-  while (!vertex_queue.empty()) {
-    int v_parent = vertex_queue.pop();
-    int current_depth = depth_values[v_parent] + 1;
-    auto eit = adjacency_list[v_parent].begin();
-    auto eit_end = adjacency_list[v_parent].end();
-    for (; eit != eit_end; eit++) {
-      int to_vertex = *eit;
-      if (depth_values[to_vertex] == UNVISITED) {
-        depth_values[to_vertex] = current_depth;
-        vertex_queue.push(to_vertex);
-      }
-    }
-  }
-  return UNVISITED;
 }
 
 // We create the graph of strongly connected graphs, while processing each
@@ -410,7 +448,7 @@ void createGraphOfStronglyConnectedComponents(
 #pragma omp parallel
   {
     std::vector<int> temp_buffer(num_components);
-    std::vector<bool> has_edge_to_component(num_components, false);
+    std::vector<bool> found_edge_to_component(num_components, false);
 #pragma omp for
     for (int component = 0; component < num_components; component++) {
       int num_out_edges = 0;
@@ -423,8 +461,8 @@ void createGraphOfStronglyConnectedComponents(
         for (; eit != eit_end; eit++) {
           int to_component = vertex_to_component_map[*eit];
           if ((to_component != component) &&
-              (!has_edge_to_component[to_component])) {
-            has_edge_to_component[to_component] = true;
+              (!found_edge_to_component[to_component])) {
+            found_edge_to_component[to_component] = true;
             temp_buffer[num_out_edges++] = to_component;
           }
         }
@@ -436,7 +474,7 @@ void createGraphOfStronglyConnectedComponents(
       // We have to reinitialize the hash table, instead of using std::clear
       // this is faster as we clear only the set elements.
       for (int i = 0; i < num_out_edges; i++) {
-        has_edge_to_component[temp_buffer[i]] = false;
+        found_edge_to_component[temp_buffer[i]] = false;
       }
     }
   }
