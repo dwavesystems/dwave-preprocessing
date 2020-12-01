30b0f31b3fe479e6bc1587dfa6b60a5be39a16cf
Anil Mahmud <amahmud@dwavesys.com>
2020-11-30 23:36:37 -0800

    NFC

--- roof_duality/fix_variables.hpp
+++ roof_duality/fix_variables.hpp
@@ -25,6 +25,8 @@
 
 namespace fix_variables_ {
 
+typedef long long int capacity_type;
+
 class compClass {
 public:
   bool operator()(const std::pair<int, int> &a, const std::pair<int, int> &b) {
@@ -35,17 +37,26 @@ public:
   }
 };
 
-typedef long long int capacity_type;
-
+// Fixes the QUBO variables.
+// When sample is set to true, it fixes only the variables which correspond to
+// strong persistencies. When sample is set to false, it tries to fix all the
+// variables corresponding to strong and weak persistencies. The variables which
+// do not contribute any coefficient to the posiform, either because their bias
+// in the QUBO/BQM is zero or they are flushed to zeroes when converted to the
+// posiform are set to 1 when sample is set to false.
 template <class PosiformInfo>
 void fixQuboVariables(PosiformInfo &posiform_info, int num_bqm_variables,
                       bool sample,
                       std::vector<std::pair<int, int>> &fixed_variables) {
   ImplicationNetwork<capacity_type> implication_network(posiform_info);
   fixed_variables.reserve(num_bqm_variables);
+
+  // Fix the variables with respect to the posiform.
   std::vector<std::pair<int, int>> fixed_variables_posiform;
   implication_network.fixVariables(fixed_variables_posiform, sample);
 
+  // There may not be 1 to 1 mapping from bqm variables to posiform variables,
+  // so we convert the posiform variables back to bqm variables.
   for (int i = 0; i < fixed_variables_posiform.size(); i++) {
     int bqm_variable = posiform_info.mapVariablePosiformToQubo(
         fixed_variables_posiform[i].first);
@@ -53,6 +64,9 @@ void fixQuboVariables(PosiformInfo &posiform_info, int num_bqm_variables,
         {bqm_variable, fixed_variables_posiform[i].second});
   }
 
+  // When we are not sampling we want to set the variables which did not
+  // contribute to the posiform as they had zero bias. They can be set to either
+  // 1 or 0. We choose 1.
   if (!sample) {
     for (int bqm_variable = 0; bqm_variable < num_bqm_variables;
          bqm_variable++) {
@@ -64,6 +78,8 @@ void fixQuboVariables(PosiformInfo &posiform_info, int num_bqm_variables,
 
   std::sort(fixed_variables.begin(), fixed_variables.end(), compClass());
 }
+
+// Fix variables for different types of BQMs.
 
 template <class V, class B>
 std::vector<std::pair<int, int>>
