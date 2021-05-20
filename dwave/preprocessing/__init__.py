0148185c8d675554fb663406de18bf22ebfdf241
Heidi Tong <htong@dwavesys.com>
2021-05-20 09:51:50 -0700

    Release 0.1.1
    Fixes
    -----
    - Fix importing of composites #10 
    - Add the composites and `roof_duality` to top level namespace #10

--- dwave/preprocessing/__init__.py
+++ dwave/preprocessing/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '0.1.1rc2'
+__version__ = '0.1.1'
 
 from dwave.preprocessing import *
 import dwave.preprocessing.composites
