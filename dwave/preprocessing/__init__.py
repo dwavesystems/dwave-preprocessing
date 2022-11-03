f88a08c2c7e8036f7ec83389f8c92b97788b7d3f
Alexander Condello <arcondello@gmail.com>
2022-11-02 17:33:43 -0700

    Update version 0.5.0.dev3 -> 0.5.0.dev4

--- dwave/preprocessing/__init__.py
+++ dwave/preprocessing/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '0.5.0.dev3'
+__version__ = '0.5.0.dev4'
 
 from dwave.preprocessing import *
 import dwave.preprocessing.composites
