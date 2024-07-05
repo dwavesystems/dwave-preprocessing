607adabd2d39d4fe2378cb06ebe2b98f6069e54f
Alexander Condello <arcondello@gmail.com>
2024-07-05 08:03:25 -0700

    Update version 0.6.5 -> 0.6.6

--- dwave/preprocessing/__init__.py
+++ dwave/preprocessing/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '0.6.5'
+__version__ = '0.6.6'
 
 from dwave.preprocessing import *
 import dwave.preprocessing.composites
